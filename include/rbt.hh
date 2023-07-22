ee1be2c059899d56446d9305f4b7c8aa2afb7805
Salpadding <m6567fc@outlook.com>
2023-07-22 11:41:37 +0800

    pratice cpp

--- include/rbt.hh
+++ include/rbt.hh
@@ -1,175 +1,161 @@
 #pragma once
-#define RED 0
-#define BLACK 1
+#include <cstdio>
+#include <queue>
 
-struct rb_node {
-    struct rb_node* parent;
-    struct rb_node* left;
-    struct rb_node* right;
+using namespace std;
+
+class rb_node {
+    static int red() { return 0; }
+    static int black() { return 1; }
+
+  public:
+    rb_node *parent;
+    rb_node *left;
+    rb_node *right;
     int color;
     int key;
     int value;
+    int size;
+
+    void rotate_left();
+    void rotate_right();
+
+    rb_node *find(int key, rb_node **last) {
+        rb_node *cur = this;
+
+        while (cur) {
+            if (last)
+                *last = cur;
+            if (key == cur->key) {
+                return cur;
+            }
+            if (key < cur->key) {
+                cur = cur->left;
+            } else {
+                cur = cur->right;
+            }
+        }
+        return cur;
+    }
+
+    void insert(int key, int value, rb_node **root) {}
+
+    void print() {
+        puts("digraph G { \n\
+graph [ratio=.48]; node \n\
+[style=filled, color=black, shape=circle, width=.6 \n\
+fontname=Helvetica, fontweight=bold, \n\
+fontcolor=white, fontsize=24, fixedsize=true];");
+        rb_node *root = this;
+        auto q = queue<rb_node *>();
+        auto red_nodes = vector<rb_node *>();
+        rb_node *cur;
+        int nil_count = 0;
+        int i;
+
+        // 打印缓冲
+        char buf[2][100];
+
+        q.push(root);
+#define fmt(x, buf)                                                            \
+    {                                                                          \
+        if (x) {                                                               \
+            sprintf(buf, "%d", x->key);                                        \
+        } else {                                                               \
+            sprintf(buf, "n%d", nil_count++);                                  \
+        }                                                                      \
+    }
+
+        while (q.size() > 0) {
+            cur = q.front();
+            q.pop();
+
+            if (cur->color == rb_node::red()) {
+                red_nodes.push_back(cur);
+            }
+
+            fmt(cur->left, buf[0]);
+            fmt(cur->left, buf[1]);
+            printf("%d -> %s, %s;\n", cur->key, buf[0], buf[1]);
+
+            if (cur->left) {
+                q.push(cur->left);
+            }
+            if (cur->right) {
+                q.push(cur->right);
+            }
+        }
+
+        if (nil_count > 0) {
+            for (i = 0; i < nil_count; i++) {
+                printf("n%d %s", i, i == nil_count - 1 ? "\n" : ",");
+            }
+            puts("[label=\"NIL\", shape=record, width=.4,height=.25, "
+                 "fontsize=16];\n");
+        }
+
+        if (red_nodes.size() > 0) {
+            for (i = 0; i < red_nodes.size(); i++) {
+                printf("%d %s", red_nodes[i],
+                       i == red_nodes.size() - 1 ? "\n" : ",");
+            }
+            puts("[fillcolor=red]\n");
+        }
+    }
+
+    void fix_insert(rb_node **root) {
+        rb_node *cur = this;
+        rb_node *p = cur->parent;
+        rb_node *uncle;
+        rb_node *grandpa;
+
+    retry:
+        if (!p) {
+            this->color = rb_node::black();
+            return;
+        }
+
+        if (p->color == rb_node::black()) {
+            return;
+        }
+
+        grandpa = p->parent;
+        if (p == grandpa->left) {
+            uncle = grandpa->right;
+        } else {
+            uncle = grandpa->left;
+        }
+        if (uncle && uncle->color == rb_node::red()) {
+            p->color = rb_node::black();
+            uncle->color = rb_node::black();
+            grandpa->color = rb_node::red();
+            cur = grandpa;
+            goto retry;
+        }
+
+        if (grandpa == *root) {
+            *root = p;
+        }
+        if (p == grandpa->left) {
+            if (cur == p->right) {
+                p->rotate_left();
+                p = cur;
+            }
+            p->color = rb_node::black();
+            grandpa->color = rb_node::red();
+            if (grandpa == *root) {
+                *root = p;
+            }
+            grandpa->rotate_right();
+        } else {
+            if (cur == p->left) {
+                p->rotate_right();
+                p = cur;
+            }
+            p->color = rb_node::black();
+            grandpa->color = rb_node::red();
+            grandpa->rotate_left();
+        }
+        return;
+    }
 };
-
-// 左旋
-void rotate_left(struct rb_node* node) {
-    if (! node->right) return;
-    struct rb_node* y = node->right;
-    y->parent = node->parent;
-    if (node->parent) {
-        if (node == node->parent->left) {
-            y->parent->left = y;
-        } else {
-            y->parent->right = y;
-        }
-    }
-    struct rb_node* yl = y->left;
-    node->parent = y;
-    y->left = node;
-    node->right = yl;
-    if (yl) {
-        yl->parent = node;
-    }
-}
-
-// 右旋
-void rotate_right(struct rb_node* node) {
-    if (!node->left) return;
-    struct rb_node* y = node->left;
-    y->parent = node->parent;
-    if (node->parent) {
-        if (node == node->parent->left) {
-            y->parent->left = y;
-        } else {
-            y->parent->right = y;
-        }
-    }
-
-    struct rb_node* yr = y->right;
-    node->parent = y;
-    y->right = node;
-    node->left = yr;
-    if (yr) {
-        yr->parent = node;
-    }
-}
-
-// 修复红黑树
-void fix_insert(struct rb_node* node, struct rb_node** root) {
-    if (!node->parent) {
-        node->color = BLACK;
-        return;
-    }
-
-    if (node->parent->color == BLACK) return;
-
-    struct rb_node* parent = node->parent;
-
-    struct rb_node* uncle;
-    if (parent == parent->parent->left) {
-        uncle = parent->parent->right;
-    } else {
-        uncle = parent->parent->left;
-    }
-
-    if (uncle && uncle->color == RED) {
-        parent->color = BLACK;
-        uncle->color = BLACK;
-        parent->parent->color = RED;
-        return fix_insert(parent->parent, root);
-    }
-
-    if (parent == parent->parent->left) {
-        // LR -> LL
-        if (node == parent->right) {
-            rotate_left(parent);
-            parent = node;
-        }
-        if (parent->parent == *root) {
-            *root = parent;
-        }
-        parent->color = BLACK;
-        parent->parent->color = RED;
-        rotate_right(parent->parent);
-    } else {
-        if (node == parent->left) {
-            rotate_right(parent);
-            parent = node;
-        }
-        if (parent->parent == *root) {
-            *root = parent;
-        }        
-        parent->color = BLACK;
-        parent->parent->color = RED;
-        rotate_left(parent->parent);
-    }
-}
-
-// 查找
-struct rb_node* find(struct rb_node* node, int key, struct rb_node** last) {
-    if (!node) return NULL;
-    if (last) *last = node;
-    if (key == node->key) return node;
-    if (key < node->key) {
-        return find(node->left, key, last);
-    }
-    return find(node->right, key, last);
-}
-
-void insert(struct rb_node** root, int key, int value) {
-    if (!(*root)) {
-        *root = calloc(1, sizeof(struct rb_node));
-        (*root)->color = BLACK;
-        (*root)->key = key;
-        (*root)->value = value;
-        return;
-    }
-    struct rb_node* parent;
-    struct rb_node* found = find(*root, key, &parent);
-    if (found) {
-        found->value = value;
-        return;
-    }
-
-    struct rb_node* new_node = calloc(1, sizeof(struct rb_node));
-    new_node->key = key;
-    new_node->value = value;
-    new_node->parent = parent;
-    if (key < parent->key) {
-        parent->left = new_node;
-    } else {
-        parent->right = new_node;
-    }
-    fix_insert(new_node, root);
-}
-
-void print_tree(struct rb_node* n) {
-    if (!n) {
-        return;
-    }
-    print_tree(n->left);
-    printf("%d %d\n", n->key, n->value);
-    print_tree(n->right);
-}
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    struct rb_node* root;
-    struct rb_node* found;
-    for(int i = 0; i < numsSize; i++) {
-        found = find(root, nums[i], NULL);
-        if (found) {
-            int* ret = calloc(2, sizeof(int));
-            ret[0] = found->value;
-            ret[1] = i;
-            *returnSize= 2;
-            return ret;
-        }
-        insert(&root, target - nums[i], i);
-    }
-    return NULL;
-}
-
-
